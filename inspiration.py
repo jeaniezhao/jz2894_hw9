ec04dd33977577d865fbfa419f733142a18afc42
Jeanie Zhao <jz2894@columbia.edu>
2018-04-24 22:37:34 +0000

    Add FLASK file

---
+++ inspiration.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def random.quotes():
+        with open('inspiration.txt', 'r', encoding='utf-8') as fp:
+            data - fp.read().split('\n')
+        return(random.choice(data))
+
+
+
+if __name__='__main__':
+    app.run()
+
+
---
+++ jz2894_hw9.py
@@ -0,0 +1,12 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from random import randint
+
+app = Flask(__name__)
+ 
+@app.route("/")
+def quot():
+    text_file = open('inspiration.txt', "r")
+    data = text_file.read().split('\n')
+    randomNumber = randint(0,len(data)-1) 
+    quote = data[randomNumber] 
+    return render_template('test.html',**locals())
